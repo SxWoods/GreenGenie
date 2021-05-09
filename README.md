1bdcd00a21e4f4a90f35970a03fd3129abf6aaae
Brian Sales <mr.brian.mrs.kathleen@gmail.com>
2021-05-08 23:53:17 -0500

    added login page with homepage

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # GreenGenie
 Weed growing application
+
+Make sure to run `npm install -g gulp` before running `npm install`
--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -6,14 +6,14 @@ router.get('/', async (req, res) => {
     res.render('homepage');
 });
 
-// router.get('/login', (req, res) => {
+router.get('/login', (req, res) => {
 //   if (req.session.logged_in) {
 //     res.redirect('/');
 //     return;
 //   }
 
-//   res.render('login');
-// });
+  res.render('login');
+});
 
 module.exports = router;
 
