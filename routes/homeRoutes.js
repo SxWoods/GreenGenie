33b7ebebf504375240f03d0949ca382e1b818295
Tim O'Malley <tomalley24@gmail.com>
2021-05-09 16:31:37 -0500

    added withAuth to / route and now redirects to login page if you are not logged in

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 //const { User } = require('../models');
-//const withAuth = require('../utils/auth');
+const withAuth = require('../utils/auth');
 
-router.get('/', async (req, res) => {
+router.get('/', withAuth, async (req, res) => {
     res.render('homepage');
 });
 
