ee111a4a8857a979d293816bb5426a9ca079033a
Tim O'Malley <tomalley24@gmail.com>
2021-05-10 19:45:42 -0500

    sign up now functions and adds users to the DB

--- public/js/login.js
+++ public/js/login.js
@@ -13,7 +13,7 @@ const loginFormHandler = async (event) => {
     });
 
     if (response.ok) {
-      document.location.replace('/profile');
+      document.location.replace('/');
     } else {
       alert(response.statusText);
     }
@@ -23,19 +23,19 @@ const loginFormHandler = async (event) => {
 const signupFormHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector('#uname-su').value.trim();
+  const username = document.querySelector('#uname-su').value.trim();
   const email = document.querySelector('#email-su').value.trim();
   const password = document.querySelector('#pword-su').value.trim();
 
-  if (name && email && password) {
-    const response = await fetch('/api/users', {
+  if (username && email && password) {
+    const response = await fetch('/api/users/signUp', {
       method: 'POST',
-      body: JSON.stringify({ name, email, password }),
+      body: JSON.stringify({ username, email, password }),
       headers: { 'Content-Type': 'application/json' },
     });
 
     if (response.ok) {
-      document.location.replace('/profile');
+      document.location.replace('/');
     } else {
       alert(response.statusText);
     }
@@ -43,11 +43,11 @@ const signupFormHandler = async (event) => {
 };
 
 document
-  .querySelector('.login-form')
+  .querySelector('.login-container')
   .addEventListener('submit', loginFormHandler);
 
 document
-  .querySelector('.signup-form')
+  .querySelector('.signup-container')
   .addEventListener('submit', signupFormHandler);
 
   
