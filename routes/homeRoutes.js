e5b397614c73a5435ce068883d9eabcf7f8afca1
Tim O'Malley <tomalley24@gmail.com>
2021-05-10 19:05:45 -0500

    routes update

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,8 +1,8 @@
-const router = require('express').Router();
+ const router = require('express').Router();
 //const { User } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/', withAuth, async (req, res) => {
+router.get('/', async (req, res) => { 
     res.render('homepage');
 });
 
